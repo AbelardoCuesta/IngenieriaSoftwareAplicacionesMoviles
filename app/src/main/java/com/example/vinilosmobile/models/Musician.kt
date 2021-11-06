044e7cf9c56a077f926db627eeb1f54323575d39
Nilson Parra <np.martinez@mensajerosurbanos.com>
2021-11-05 21:37:50 -0500

    modelo y reporsitory de Musicos

---
+++ app/src/main/java/com/example/vinilosmobile/models/Musician.kt
@@ -0,0 +1,10 @@
+package com.example.vinilosmobile.models
+
+data class Musician(
+    val musicianId: Int,
+    val name:String,
+    val image: String,
+    val description: String,
+    val birthDate: String
+
+)
