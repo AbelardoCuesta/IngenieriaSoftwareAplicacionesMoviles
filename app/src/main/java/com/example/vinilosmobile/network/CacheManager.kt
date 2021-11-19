0439b23713b8100ea5742ec50229360d2c364534
Nilson Parra <np.martinez@mensajerosurbanos.com>
2021-11-18 23:07:36 -0500

    cache en musicos detalle

--- app/src/main/java/com/example/vinilosmobile/network/CacheManager.kt
+++ app/src/main/java/com/example/vinilosmobile/network/CacheManager.kt
@@ -5,8 +5,9 @@ import com.example.vinilosmobile.models.Album;
 class CacheManager(context: Context) {
     companion object {
         var instance: CacheManager? = null
-        const val APP_SPREFS = "com.example.vinilosmobile.app"
         const val ALBUMS_SPREFS = "com.example.vinilosmobile.albums"
+        const val MUSICIANS_SPREFS = "com.example.vinilosmobile.musicians"
+        const val COLLECTORS_SPREFS = "com.example.vinilosmobile.collectors"
 
 
         fun getPrefs(context: Context, name:String): SharedPreferences {
