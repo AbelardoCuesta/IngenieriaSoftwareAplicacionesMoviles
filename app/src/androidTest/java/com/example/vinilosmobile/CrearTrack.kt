316ed9b01cdf0eb0cd3d667cc34b6b8f4d237c54
karen xiomara <k.correa@uniandes.edu.co>
2021-12-02 22:14:18 -0500

    Pruebas de las historias finales

---
+++ app/src/androidTest/java/com/example/vinilosmobile/CrearTrack.kt
@@ -0,0 +1,155 @@
+package com.example.vinilosmobile
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CrearTrack {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun crearTrack() {
+        val appCompatImageButton = onView(
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+
+        val navigationMenuItemView = onView(
+            allOf(
+                withId(R.id.nav_albums),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        navigationMenuItemView.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.albumsRv),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    2
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+        onView(isRoot()).perform(waitFor(3000))
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab), withContentDescription("Crear un track"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.saveTrack), withText("Crear track"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    4
+                )
+            )
+        )
+        materialButton.perform(scrollTo(), click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val textView = onView(
+            allOf(
+                withId(R.id.textinput_error), withText("Debe ingresar el nombre"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Debe ingresar el nombre")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    fun waitFor(delay: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $delay milliseconds"
+            override fun perform(uiController: UiController, v: View?) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+        }
+    }
+}
